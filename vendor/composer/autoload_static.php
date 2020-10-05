dffe3a6da194a4e54a57245297a661e9936332e8
aruiz <christian922017@gmail.com>
2020-10-05 00:22:07 -0500

    estilos

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -27,6 +27,7 @@ class ComposerStaticInit5e5b3584312e2aff6ac7dbbec10caa7a
         '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
         'b6ec61354e97f32c0ae683041c78392a' => __DIR__ . '/..' . '/scrivo/highlight.php/HighlightUtilities/functions.php',
         'ed962a97bd972bc82007176b647d4e36' => __DIR__ . '/..' . '/facade/ignition/src/helpers.php',
+        '95b3813a3525f3ec11f46853708a60c5' => __DIR__ . '/../..' . '/app/Helpers/Navigation.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -447,6 +448,7 @@ class ComposerStaticInit5e5b3584312e2aff6ac7dbbec10caa7a
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Auth\\VerificationController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/VerificationController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\CheckForMaintenanceMode' => __DIR__ . '/../..' . '/app/Http/Middleware/CheckForMaintenanceMode.php',
